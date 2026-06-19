1a9c98de251bb7c59c081329f8d90bfe7e150799
Wolterink99 <wolterink99@github.com>
2026-06-19 20:25:56 +0200

    Update deploy script commit hash

--- deploy_immutable_cdn.cjs
+++ deploy_immutable_cdn.cjs
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NWIyYTA4MzhjOWQ0NjI2Yjc5NTY5NGU1Mzk0ZmU2ZCIsImlhdCI6MTc4MDIyODEwMiwiZXhwIjoyMDk1NTg4MTAyfQ.c-gOuNc3AezEImPkxMvuBeTPVbfpx8CzWqcixCpflM4";
 const url = "wss://84wgzzzm8ai8igpemwargw2qsuihp9ww.ui.nabu.casa/api/websocket";
-const commitHash = "24c0ddadb148edeae6ed6a13a000c6c3b4109dd4";
+const commitHash = "aa9eca017df489bae6825938b14f879f4369108a";
 
 const ws = new WebSocket(url);
 
