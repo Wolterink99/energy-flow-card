540a009ab52c0b51d737dd162d33243a534961a7
Wolterink99 <wolterink99@github.com>
2026-06-19 19:37:34 +0200

    Update deploy script commit hash for weather popup

--- deploy_immutable_cdn.cjs
+++ deploy_immutable_cdn.cjs
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NWIyYTA4MzhjOWQ0NjI2Yjc5NTY5NGU1Mzk0ZmU2ZCIsImlhdCI6MTc4MDIyODEwMiwiZXhwIjoyMDk1NTg4MTAyfQ.c-gOuNc3AezEImPkxMvuBeTPVbfpx8CzWqcixCpflM4";
 const url = "wss://84wgzzzm8ai8igpemwargw2qsuihp9ww.ui.nabu.casa/api/websocket";
-const commitHash = "2bd360bb62eb62106678faf18fcf2909ff356d62";
+const commitHash = "11efdbfcaa8ecdb27c37123931163dd1f50daee7";
 
 const ws = new WebSocket(url);
 
