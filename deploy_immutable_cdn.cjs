d6b3873cea2d76c1117cba94d7e1cb6248d46605
Wolterink99 <wolterink99@github.com>
2026-06-19 19:29:05 +0200

    Update deploy script commit hash for weather HUD card

--- deploy_immutable_cdn.cjs
+++ deploy_immutable_cdn.cjs
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NWIyYTA4MzhjOWQ0NjI2Yjc5NTY5NGU1Mzk0ZmU2ZCIsImlhdCI6MTc4MDIyODEwMiwiZXhwIjoyMDk1NTg4MTAyfQ.c-gOuNc3AezEImPkxMvuBeTPVbfpx8CzWqcixCpflM4";
 const url = "wss://84wgzzzm8ai8igpemwargw2qsuihp9ww.ui.nabu.casa/api/websocket";
-const commitHash = "09c56bb69175760800be2cdacf4fe5c8e61b3d8e";
+const commitHash = "62307c294651ce400b05758265b0351a48954858";
 
 const ws = new WebSocket(url);
 
