59d2857f2fc8e7f81f25f89d86e5b6cc688f5f1d
Wolterink99 <wolterink99@github.com>
2026-06-19 19:32:14 +0200

    Update deploy script commit hash for weather card top-right positioning

--- deploy_immutable_cdn.cjs
+++ deploy_immutable_cdn.cjs
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NWIyYTA4MzhjOWQ0NjI2Yjc5NTY5NGU1Mzk0ZmU2ZCIsImlhdCI6MTc4MDIyODEwMiwiZXhwIjoyMDk1NTg4MTAyfQ.c-gOuNc3AezEImPkxMvuBeTPVbfpx8CzWqcixCpflM4";
 const url = "wss://84wgzzzm8ai8igpemwargw2qsuihp9ww.ui.nabu.casa/api/websocket";
-const commitHash = "62307c294651ce400b05758265b0351a48954858";
+const commitHash = "2bd360bb62eb62106678faf18fcf2909ff356d62";
 
 const ws = new WebSocket(url);
 
