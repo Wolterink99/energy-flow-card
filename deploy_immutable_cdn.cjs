3cafbce8a00009be78b097ad67c0d0f82b88dc37
Wolterink99 <wolterink99@github.com>
2026-06-21 12:08:39 +0200

    chore: update deployment commit hash to 3d82894

--- deploy_immutable_cdn.cjs
+++ deploy_immutable_cdn.cjs
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NWIyYTA4MzhjOWQ0NjI2Yjc5NTY5NGU1Mzk0ZmU2ZCIsImlhdCI6MTc4MDIyODEwMiwiZXhwIjoyMDk1NTg4MTAyfQ.c-gOuNc3AezEImPkxMvuBeTPVbfpx8CzWqcixCpflM4";
 const url = "wss://84wgzzzm8ai8igpemwargw2qsuihp9ww.ui.nabu.casa/api/websocket";
-const commitHash = "0fff33831685ff9e8b91c0ab481885a91213249b";
+const commitHash = "3d8289416773d514803ce1d43778dc88c503b46f";
 
 const ws = new WebSocket(url);
 
