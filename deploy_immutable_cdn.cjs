2a2c64297436b80f7faffc16c8e1f3630be2eb02
Wolterink99 <wolterink99@github.com>
2026-06-21 17:59:45 +0200

    chore: update deployment script to deploy 0bea5d9 with temperature override

--- deploy_immutable_cdn.cjs
+++ deploy_immutable_cdn.cjs
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NWIyYTA4MzhjOWQ0NjI2Yjc5NTY5NGU1Mzk0ZmU2ZCIsImlhdCI6MTc4MDIyODEwMiwiZXhwIjoyMDk1NTg4MTAyfQ.c-gOuNc3AezEImPkxMvuBeTPVbfpx8CzWqcixCpflM4";
 const url = "wss://84wgzzzm8ai8igpemwargw2qsuihp9ww.ui.nabu.casa/api/websocket";
-const commitHash = "6ea8e8b755d95c0e71d927729b28098e19417b7e";
+const commitHash = "0bea5d9b41422aa96aab9db789bbd7b62cc35021";
 
 const ws = new WebSocket(url);
 
