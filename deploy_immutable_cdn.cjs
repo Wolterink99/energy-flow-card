f4ddc3f25c8f2f7fefa146cfefcab62ed310242f
Wolterink99 <wolterink99@github.com>
2026-06-22 19:18:19 +0200

    chore: update deployment script to deploy 0b500ba

--- deploy_immutable_cdn.cjs
+++ deploy_immutable_cdn.cjs
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NWIyYTA4MzhjOWQ0NjI2Yjc5NTY5NGU1Mzk0ZmU2ZCIsImlhdCI6MTc4MDIyODEwMiwiZXhwIjoyMDk1NTg4MTAyfQ.c-gOuNc3AezEImPkxMvuBeTPVbfpx8CzWqcixCpflM4";
 const url = "wss://84wgzzzm8ai8igpemwargw2qsuihp9ww.ui.nabu.casa/api/websocket";
-const commitHash = "0bea5d9b41422aa96aab9db789bbd7b62cc35021";
+const commitHash = "0b500ba2f794ad406c03ebfee4851d53427afbf3";
 
 const ws = new WebSocket(url);
 
