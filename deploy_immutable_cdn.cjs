f6e51488a68a5cf831a0fc5a2ec246e350f08619
Wolterink99 <wolterink99@github.com>
2026-06-23 19:31:51 +0200

    Update deploy script commit hash for window reflection fix

--- deploy_immutable_cdn.cjs
+++ deploy_immutable_cdn.cjs
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NWIyYTA4MzhjOWQ0NjI2Yjc5NTY5NGU1Mzk0ZmU2ZCIsImlhdCI6MTc4MDIyODEwMiwiZXhwIjoyMDk1NTg4MTAyfQ.c-gOuNc3AezEImPkxMvuBeTPVbfpx8CzWqcixCpflM4";
 const url = "wss://84wgzzzm8ai8igpemwargw2qsuihp9ww.ui.nabu.casa/api/websocket";
-const commitHash = "205d3db29f233e781cfca670afd8f107bfd383af";
+const commitHash = "3dd0270664e70e0be1656f11ced5bd84ce1639a7";
 
 const ws = new WebSocket(url);
 
